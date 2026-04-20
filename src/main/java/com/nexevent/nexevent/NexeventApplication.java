dfb46af74ae0ac057c07b15f71e6626e4ceb4e14
Nguyen Duc Thien <nguyenducthienlq1@gmail.com>
2026-04-21 00:01:20 +0700

    create project

---
+++ src/main/java/com/nexevent/nexevent/NexeventApplication.java
@@ -0,0 +1,13 @@
+package com.nexevent.nexevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexeventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexevent/nexevent/NexeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexevent.nexevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
