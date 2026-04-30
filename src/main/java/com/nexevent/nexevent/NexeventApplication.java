3958c71343ccb23518afa6abe0da2fd431fe14e0
Nguyen Duc Thien <nguyenducthienlq1@gmail.com>
2026-04-30 14:49:34 +0700

    feat(event): create database by flyway

--- src/main/java/com/nexevent/nexevent/NexeventApplication.java
+++ src/main/java/com/nexevent/nexevent/NexeventApplication.java
@@ -3,7 +3,6 @@ package com.nexevent.nexevent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableAsync
